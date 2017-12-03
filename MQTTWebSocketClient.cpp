9ff6f3e276cd3f4e81b48ff1661b262170f229c8
Vojtech Vecera <vecera.vojta@seznam.cz>
2017-12-03 23:34:37 +0100

    [ADD] SSL for WebSockets

--- MQTTWebSocketClient.cpp
+++ MQTTWebSocketClient.cpp
@@ -8,19 +8,22 @@ void MQTTWebSocketClient::webSocketEvent(WStype_t type, uint8_t * payload, size_
 
     switch(type) {
         case WStype_DISCONNECTED:
-            DEBUG_WEBSOCKET_MQTT("[WSc] Disconnected!\n");
-			MQTTWebSocketClient::instance->stop ();			
+            Serial.println("[WSc] Disconnected!");
+			MQTTWebSocketClient::instance->stop ();
             break;
-        case WStype_CONNECTED:            
-            DEBUG_WEBSOCKET_MQTT("[WSc] Connected to url: %s\n",  payload);
+        case WStype_CONNECTED:
+            Serial.print("[WSc] Connected to url:");
+            Serial.println(*payload);
 			MQTTWebSocketClient::instance->_connected = true;
             break;
         case WStype_TEXT:
-            DEBUG_WEBSOCKET_MQTT("[WSc] get text: %s\n", payload);
+            Serial.print("[WSc] get text:");
+            Serial.println(*payload);
             MQTTWebSocketClient::instance->putMessage (payload, length);
             break;
         case WStype_BIN:
-            DEBUG_WEBSOCKET_MQTT("[WSc] get binary length: %u\n", length);
+            Serial.print("[WSc] get binary length:");
+          Serial.println(length);
             //hexdump(payload, length);
             MQTTWebSocketClient::instance->putMessage (payload, length);
             break;
@@ -29,18 +32,19 @@ void MQTTWebSocketClient::webSocketEvent(WStype_t type, uint8_t * payload, size_
 
 //constructor
 MQTTWebSocketClient::MQTTWebSocketClient (unsigned int bufferSize, unsigned long connectionTimeout) {
+    useSSL = false;
     _connectionTimeout = connectionTimeout;
     AWSWebSocketClient:instance = this;
     onEvent(MQTTWebSocketClient::webSocketEvent);
     path = NULL;
-	_connected = false;	
+    _connected = false;
     bb.init (bufferSize); //1000 bytes of circular buffer... maybe it is too big
 }
 
 //destructor
 MQTTWebSocketClient::~MQTTWebSocketClient(void) {
     if (path != NULL)
-        delete[] path;	
+        delete[] path;
 }
 
 /* Converts an integer value to its hex character*/
@@ -55,11 +59,11 @@ char* url_encode(const char* str) {
   const char* pstr = str;
   char *buf = (char*) malloc(strlen(str) * 3 + 1), *pbuf = buf;
   while (*pstr) {
-    if (isalnum(*pstr) || *pstr == '-' || *pstr == '_' || *pstr == '.' || *pstr == '~') 
+    if (isalnum(*pstr) || *pstr == '-' || *pstr == '_' || *pstr == '.' || *pstr == '~')
       *pbuf++ = *pstr;
-    else if (*pstr == ' ') 
+    else if (*pstr == ' ')
       *pbuf++ = '+';
-    else 
+    else
       *pbuf++ = '%', *pbuf++ = to_hex(*pstr >> 4), *pbuf++ = to_hex(*pstr & 15);
     pstr++;
   }
@@ -76,23 +80,25 @@ MQTTWebSocketClient& MQTTWebSocketClient::setPath(const char * path) {
 	return *this;
 }
 
-int MQTTWebSocketClient::connect(IPAddress ip, uint16_t port) {
-	return -1; // not implemented
+int MQTTWebSocketClient::connect(IPAddress ip, uint16_t port){
+  return -1; // not implemented
 }
 
 int MQTTWebSocketClient::connect(const char *host, uint16_t port) {
 	//make sure it is disconnect first
 	  const char* protocol = "mqtt";
-	  stop ();	
+	  stop ();
 	  char* path = this->path;
 	  //just need to free path if it was generated to connect to AWS
 	  bool freePath = false;
-
-	  begin(host, port, path, protocol);
+    if (useSSL == true)
+		  beginSSL (host,port,path,"",protocol);
+	  else
+      begin(host, port, path, protocol);
 	  long now = millis ();
 	  while ( (millis ()-now) < _connectionTimeout) {
-		  loop ();		  
-		  if (connected () == 1) {		  
+		  loop ();
+		  if (connected () == 1) {
 			  if (freePath == true)
 				delete[] path;
 			  return 1;
@@ -100,13 +106,13 @@ int MQTTWebSocketClient::connect(const char *host, uint16_t port) {
 		  delay (10);
 	  }
 	  if (freePath == true)
-		delete[] path;
+      delete[] path;
 	  return 0;
 }
 
 //store messages arrived by websocket layer to be consumed by mqtt layer through the read funcion
 void MQTTWebSocketClient::putMessage (byte* buffer, int length) {
-	bb.push (buffer,length);	
+	bb.push (buffer,length);
 }
 
 size_t MQTTWebSocketClient::write(uint8_t b) {
@@ -148,20 +154,19 @@ int MQTTWebSocketClient::read(uint8_t *buf, size_t size) {
 	if (bb.getSize()<s)
 		s = bb.getSize ();
 
-	bb.pop (buf,s);	
+	bb.pop (buf,s);
 	return s;
-};
+}
 
 int MQTTWebSocketClient::peek() {
 	return bb.peek ();
 }
 
 void MQTTWebSocketClient::flush() {
-
 }
 
 void MQTTWebSocketClient::stop() {
-	if (_connected == true) {		
+	if (_connected == true) {
 		_connected = false;
 		bb.clear ();
 	}
@@ -170,8 +175,17 @@ void MQTTWebSocketClient::stop() {
 
 uint8_t MQTTWebSocketClient::connected() {
 return _connected;
-};
+}
 
 MQTTWebSocketClient::operator bool() {
 	return _connected;
-};
+}
+
+bool MQTTWebSocketClient::getUseSSL () {
+  return useSSL;
+}
+
+MQTTWebSocketClient& MQTTWebSocketClient::setUseSSL (bool value) {
+  useSSL = value;
+  return *this;
+}
--- MQTTWebSocketClient.h
+++ MQTTWebSocketClient.h
@@ -1,60 +1,74 @@
 #ifndef _MQTTWEBSOCKETCLIENT_H_
 #define _MQTTWEBSOCKETCLIENT_H
 
-#include <Arduino.h>
 #include <Stream.h>
-#include "Client.h"
-#include "WebSocketsClient.h"
+#include <Client.h>
+#include <WebSocketsClient.h>
 #include "CircularByteBuffer.h"
-#include "Utils.h"
 
 static const int HASH_HEX_LEN2 = 64;
 
 #ifndef DEBUG_WEBSOCKET_MQTT
+#ifdef USE_SERIAL
+#define DEBUG_WEBSOCKET_MQTT(...) USE_SERIAL.printf( _VA_ARGS__ )
+#else
 #define DEBUG_WEBSOCKET_MQTT(...)
+#endif
 #define NODEBUG_WEBSOCKET_MQTT
 #endif
 
 class MQTTWebSocketClient : public Client, public WebSocketsClient {
-public:
+  public:
 
-  //bufferSize defines the size of the circular byte buffer that provides the interface between messages arrived in websocket layer and byte reads from mqtt layer	
-  MQTTWebSocketClient (unsigned int bufferSize = 1000, unsigned long connectionTimeout = 50000);
-  ~MQTTWebSocketClient();
+    //bufferSize defines the size of the circular byte buffer that provides the interface between messages arrived in websocket layer and byte reads from mqtt layer
+    MQTTWebSocketClient (unsigned int bufferSize = 1000, unsigned long connectionTimeout = 50000);
+    ~MQTTWebSocketClient();
 
-  int connect(IPAddress ip, uint16_t port);
-  int connect(const char *host, uint16_t port);
+    int connect(IPAddress ip, uint16_t port);
+    int connect(const char *host, uint16_t port);
 
-  void putMessage (byte* buffer, int length);
-  size_t write(uint8_t b);
-  size_t write(const uint8_t *buf, size_t size);
-  int available();
-  int read();
-  int read(uint8_t *buf, size_t size);
+    void putMessage (byte* buffer, int length);
+    size_t write(uint8_t b);
+    size_t write(const uint8_t *buf, size_t size);
+    int available();
+    int read();
+    int read(uint8_t *buf, size_t size);
 
-  int peek();
-  void flush();
-  void stop();
-  uint8_t connected() ;
-  operator bool();
+    int peek();
+    void flush();
+    void stop();
+    uint8_t connected() ;
+    operator bool();
 
-  MQTTWebSocketClient& setPath(const char * path);
-  
-  //static instance of aws websocket client
-  static MQTTWebSocketClient* instance;
-  //keep the connection state
-  bool _connected;  
-  //websocket callback
-  static void webSocketEvent(WStype_t type, uint8_t * payload, size_t length);
-  
+    bool getUseSSL ();
+    MQTTWebSocketClient& setUseSSL (bool value);
+    MQTTWebSocketClient& setPath(const char * path);
+
+    //static instance of aws websocket client
+    static MQTTWebSocketClient* instance;
+    //keep the connection state
+    bool _connected;
+    //websocket callback
+    static void webSocketEvent(WStype_t type, uint8_t * payload, size_t length);
+
   private:
-  
-  //connection timeout 
-  unsigned long _connectionTimeout;
-  char* path;
 
-  //circular buffer to keep incoming messages from websocket
-  CircularByteBuffer bb;
+    //enable ssl... if your using mqtt over websockets at AWS IoT service, it must be enabled
+    bool useSSL;
+
+
+    //connection timeout
+    unsigned long _connectionTimeout;
+    char* path;
+
+    virtual void runCbEvent(WStype_t type, uint8_t * payload, size_t length) {
+      if(_cbEvent) {
+        _cbEvent(type, payload, length);
+      }
+    }
+
+    //circular buffer to keep incoming messages from websocket
+    CircularByteBuffer bb;
 };
 
 #endif
